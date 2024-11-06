b5826858432bcec7866a1e9c9d298d6ce25d36e3
Zhanibek <janibeksultanov97@gmail.com>
2024-11-06 20:26:47 +0500

    Create CategoryController with route and edit view files

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/',IndexController::class);
-});
+//Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+//    Route::get('/',IndexController::class);
+//});
+//
+//Route::group(['namespace' => 'App\Http\Controllers\Admin\Main','prefix' => 'admin'],function (){
+//    Route::get('/',IndexController::class);
+//
+//
+//});
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin\Main','prefix' => 'admin'],function (){
-    Route::get('/',IndexController::class);
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Auth::routes();
