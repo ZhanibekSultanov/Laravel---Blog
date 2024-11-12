f135ef8119b1fef02ec16e48a6654f85cfa511f2
Zhanibek <janibeksultanov97@gmail.com>
2024-11-12 11:20:45 +0500

    Add Show all and one Elements Action for Category with routes and view files

---
+++ app/Http/Controllers/Admin/Category/ShowController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Category;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ShowController extends Controller
+{
+    public function __invoke($category)
+    {
+        $category = Category::find($category);
+        return view('admin.categories.show',compact('category'));
+    }
+}
--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Category</h1>
+                        <h1 class="m-0">{{ $category->title }}</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -25,31 +25,21 @@
         <section class="content">
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
-                <div class="row">
-                    <div class="col-1 mb-3">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Create</a>
-                    </div>
-                </div>
+
                 <div class="row">
                     <div class="col-6">
                         <div class="card">
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
-                                    <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                    </thead>
                                     <tbody>
-                                    @foreach($categories as $category)
                                         <tr>
+                                            <td>ID</td>
                                             <td>{{ $category->id }}</td>
-                                            <td>{{ $category->title }}</td>
-                                            <td><a href="{{ route('admin.category.show',$category->id) }}"><i class="far fa-eye"></i></a></td>
                                         </tr>
-                                    @endforeach
+                                        <tr>
+                                            <td>Name</td>
+                                            <td>{{ $category->title }}</td>
+                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
